f973a7b63429d6592b582141bd700ec354a872ec
Sharon Russell <sharon.russell@comparethemarket.com>
2019-03-01 21:53:02 +0000

    React app :)

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
+const path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Exciting things coming soon!')
-})
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname+'index.html'));
+});
+
 var port = process.env.PORT || 3000; 
 
 app.listen(port, function () {
---
+++ home.jsx
@@ -0,0 +1,10 @@
+class Home extends React.Component {
+	
+	render(){
+		return ( <h1>Exciting things coming soon!</h1> );
+	}
+};
+
+ReactDOM.render(
+	<Home />, document.getElementById('home')
+);
